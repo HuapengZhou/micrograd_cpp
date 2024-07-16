d2000c9e00b08a9f323800639e52310340522387
HuapengZhou <zhouhp.me@gmail.com>
2024-07-16 21:20:16 +0800

    Add Backward Function

---
+++ src/engine/engine.cc
@@ -0,0 +1,30 @@
+#include "src/engine/engine.h"
+
+namespace engine {
+namespace {
+
+using ValuePtr = std::shared_ptr<Value>;
+}
+
+// Backward function for autograd
+void Value::Backward() {
+  std::set<ValuePtr> visited_set;
+  std::vector<ValuePtr> reversed_value_ptrs = {};
+  std::function<void(ValuePtr)> build_topo = [&](const ValuePtr &value_ptr) {
+    if (!visited_set.count(value_ptr)) {
+      visited_set.insert(value_ptr);
+      for (const ValuePtr &children_ptr : value_ptr->prev_) {
+        build_topo(children_ptr);
+      }
+      reversed_value_ptrs.push_back(value_ptr);
+    }
+  };
+  build_topo(shared_from_this());
+  grad_ = 1;
+  for (auto it = reversed_value_ptrs.rbegin(); it != reversed_value_ptrs.rend();
+       ++it) {
+    (*it)->backward_();
+  }
+}
+
+} // namespace engine
---
+++ src/engine/engine.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <set>
+#include <string>
+
+namespace engine {
+
+class Value : public std::enable_shared_from_this<Value> {
+public:
+  // Constructor
+  Value(double data, std::set<std::shared_ptr<Value>> children = {},
+        const std::string &op = "")
+      : data_(data), grad_(0), op_(op), prev_(std::move(children)) {}
+
+  // Addition operator
+  std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &other) const;
+
+  // Multiplication operator
+  std::shared_ptr<Value> operator*(const std::shared_ptr<Value> &other) const;
+
+  // Power operator
+  std::shared_ptr<Value> operator^(double other) const;
+
+  // ReLU activation function
+  std::shared_ptr<Value> ReLU() const;
+
+  // Backward function for autograd
+  void Backward();
+
+  // Negation operator
+  std::shared_ptr<Value> operator-() const;
+
+  // Subtraction operators
+  std::shared_ptr<Value> operator-(const std::shared_ptr<Value> &other) const;
+  std::shared_ptr<Value> operator-(double other) const;
+
+  // Division operators
+  std::shared_ptr<Value> operator/(const std::shared_ptr<Value> &other) const;
+  std::shared_ptr<Value> operator/(double other) const;
+
+  // Print the value and its gradient
+  friend std::ostream &operator<<(std::ostream &os, const Value &value);
+
+private:
+  double data_;
+  double grad_;
+  std::function<void()> backward_;
+  std::set<std::shared_ptr<Value>> prev_;
+  std::string op_;
+};
+
+} // namespace engine
